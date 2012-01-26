d1df31416e88a22ade9ad89edc15238eda417fc1
Ben <ben@swiftfoot.co.za>
2012-01-26 14:19:07 +0200

    Lots of css and styling

--- Intern Projects/Remarker/js/remarkerSideBar.js
+++ Intern Projects/Remarker/js/remarkerSideBar.js
@@ -41,9 +41,12 @@ function RMSideBar(){
 	function newEditHTML(){
 	return '<div class="filterBar"><p>Add a new tag:</p></div><textarea id="rm_tagContent" name="tagContent"></textarea>'+
 			'<div class="filterBar"><p>Mark As:</p></div><div id="optionsC" class="contentBox" style="overflow: hidden">'+
-			'<div class="checkBox"><input type="checkbox" name="bugBox" value="Bug" /><span> Bug </span></div>'+
-			'<div class="checkBox"><input type="checkbox" name="impBox" value="Important" /><span> Important! </span></div>'+
-			'<div class="checkBox"><input type="checkbox" name="desBox" value="Design" /><span> Design Issue </span></div>'+
+			'<div class="checkBox"><input type="checkbox" name="bugBox" value="Bug" /><span> Bug </span><span class="g">(Something doesn\'t work right!) </span></div>'+
+			'<div class="checkBox"><input type="checkbox" name="desBox" value="Design" /><span> Design Issue </span><span class="g">(Something doesn\'t look right.) </span></div>'+
+			'<div class="checkBox"><input type="checkbox" name="impBox" value="Important" /><span> Important! </span><span class="g">(Fix this ASAP!) </span></div>'+
+			'</div>'+
+			'<div class="filterBar"><p>Attach an Element:</p></div><div id="optionsC" class="contentBox" style="overflow: hidden">'+
+			'<span> No element selected. </span></div>'+
 			'</div>'+
 			'<div class="createBar"><input id="addButton" class="greenButton" type="button" value="Save" onclick=""></div>';	
 	
